3792c6fbc9d8ba05dec66a62b3da501ef7726091
渡辺創太 <w.souta.s.life@gmail.com>
2019-04-19 18:37:28 +0900

    add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-module.exports = function CheckTx(obj) {
-    //console.log(obj.mytext.value)
-    console.log('ok')
-    // var api = require('etherscan-api').init('**');
+module.exports = function CheckTx() {
+    var obj = document.document.getElementById('id_textBox1');
+    console.log(obj)
+    //console.log('ok')
+    var api = require('etherscan-api').init('BFVFPXF9FKURZ4955WRM8ADNASU9Z82EDN');
     // console.log('ok2')
     // var res = api.proxy.eth_getTransactionByHash('**')
     // console.log('ok3')
